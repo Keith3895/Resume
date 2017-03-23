7a43cc8feaf067f8fd9e3238b6108f4ec4b3c32a
keithfranklin <keith30895@gmail.com>
2017-03-23 07:51:34 +0000

    added pheno readup

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express =       require("express"),
 app         =       express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+
+
+
 app.get("/",function(req,res){
     res.render("index");
 });
@@ -14,7 +17,13 @@ app.get("/priv",function(req,res){
 app.get("/chatbot",function(req, res) {
     res.render("chatbot"); 
 });
+app.get("/pheno",function(req, res) {
+    res.render("pheno readup.ejs");
+});
 app.listen(process.env.PORT, process.env.IP,function(){
     console.log("server started");
     console.log("listening to "+process.env);
 });
+
+
+
